74ee885b0edc88faced9651ae8aa2dd258a45188
kieranbrowne <kierantbrowne@gmail.com>
2015-06-29 19:41:17 +1000

    examples callable from relative path

--- examples/example.py
+++ examples/example.py
@@ -1,4 +1,14 @@
+import sys, os, inspect
+
+# import knife from relative path
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"..")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
 import knife
+
 
 # create new colour
 col = knife.Colour()
--- examples/oilPaintMixer.py
+++ examples/oilPaintMixer.py
@@ -1,5 +1,12 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import sys, os, inspect
+
+# import knife from relative path
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"..")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
 import knife
 
 oils = [ 'cadmium yellow pale','cadmium yellow medium', 'cadmium orange', 'cadmium scarlet', 'cadmium red', 'quindcridone rose', 'dioxazine violet', 'ultramarine blue', 'thalo blue', 'thalo green', 'cadmium green', 'cadmium green pale' ]
@@ -14,6 +21,7 @@ def getMixingInstructions(desiredHue,availableHues):
         index = zip(*availableHues)[1].index(desiredHue)
         return "just use " + str(zip(*availableHues)[0][index])
     else:
+        mix = []
 
 
 
