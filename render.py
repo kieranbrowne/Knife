c310d3223a39495a68ae1b7a0da778b7dfa62b8b
Kieran Browne <kierantbrowne@gmail.com>
2015-07-02 00:15:00 +1000

    more concise

--- render.py
+++ render.py
@@ -2,31 +2,19 @@
 import Image
 import sys
 
-
 def readPalette(data):
-    RGBs = []
-    data = data.replace('[','')
-    data = data.replace(']','')
-    data = data.replace('\n','')
-    items = data.split(',')
-    for item in items:
-        if item[:3] == 'RGB':
-            RGBs.append(tuple(map(int,item[4:].split(' '))))
-            
-    print RGBs
-    return RGBs
+    data = ''.join([ i for i in data if i not in '[]\n' ])
+    RGBs = [ i for i in data.split(',') if i[:3] == 'RGB' ]
+    return [ tuple(map(int,item[4:].split(' '))) for item in RGBs ]
 
-def createImage(RGBs):
+def createImage(colours):
     width, height = 400, 200
     img = Image.new('RGB', (width,height), 'white')
     pixels = img.load()
-    for x in range(width):
-        for y in range(height):
-            if RGBs:
-                index = int(x/(float(width)/len(RGBs)))
-                pixels[x,y] = RGBs[index]
+    for x in range(width): 
+        for y in range(height): 
+            if colours:
+                pixels[x,y] = colours[int(x/(float(width)/len(colours)))]
     img.save('palette.png')
 
-
-data = sys.stdin.readline()
-createImage(readPalette(data))
+createImage(readPalette(sys.stdin.readline()))
