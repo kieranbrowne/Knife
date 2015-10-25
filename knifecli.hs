ce4171ac2677e060865e34fb21385033b314c752
Kieran Browne <kierantbrowne@gmail.com>
2015-10-25 13:50:12 +1100

    fixed stock assignment

--- knifecli.hs
+++ knifecli.hs
@@ -12,14 +12,20 @@ main = do
 interpret :: [String] -> String
 interpret args = format $ map colourmodel $ descriptors $ harmony $ colour 
     where colour 
-               | any (`elem` arggroups) stockNames = (map snd stock) !! (head $ map fromJust $ filter (/=Nothing) $ map (`elemIndex` arggroups) stockNames)
+               | any (`elem` arggroups) stockNames = (map snd stock) !! (fromJust $ elemIndex (head $ filter (`elem` arggroups) stockNames) stockNames)
                | otherwise = getStock "wheat"
                where arggroups = (map (\(a,b)->a++" "++b) $ zip args (tail args)) ++ (map (\(a,b,c)->a++" "++b++" "++c) $ zip3 args (tail args) (tail $ tail args))
                      stockNames = (map fst stock)
           harmony  
+               | "complementary" `elem` args && "split" `elem` args = splitComplementary
                | "complementary" `elem` args = complementary
+               | "analogous" `elem` args = analogous
+               | "triadic" `elem` args = triadic
+               -- | "tetradic" `elem` args = tetradic -- this wont work except with two colour inputs
                | otherwise = complementary
-          descriptors = cool . brilliant
+          descriptors 
+               | "analogous" `elem` args = cool . brilliant
+               | otherwise = id
           colourmodel
                | "hsv" `elem` args = toHSV
                | "cmyk" `elem` args = toCMYK
